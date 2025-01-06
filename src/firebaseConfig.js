99cc2a4ebcc05c7720e568bd7545ab1124df053e
FGS <153161613+Tiestof@users.noreply.github.com>
2025-01-05 22:28:06 -0300

    Update firebaseConfig.js
    por seguridad

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFunctions } from "firebase/functions";
 
 //  configuracion  para Firebase 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3Q21EHXVI1urOAA_7EOQM6lpZGrG8j7g",
-    authDomain: "gestion-proyecto-3630e.firebaseapp.com",
-    projectId: "gestion-proyecto-3630e",
+    apiKey: "Eliminada",
+    authDomain: "Eliminada",
+    projectId: "Eliminada",
     storageBucket: "gestion-proyecto-3630e.firebasestorage.app",
-    messagingSenderId: "547684576364",
-    appId: "1:547684576364:web:6f97981a6aaa626e623c49"
+    messagingSenderId: "Eliminada",
+    appId: "1:Eliminada"
   };
 
   const app = initializeApp(firebaseConfig);
